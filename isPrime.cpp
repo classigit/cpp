6a9d5820341ad5337cad724dced9fa985e50a601
John Doe <classify@gmail.com>
2018-04-26 12:29:49 +0300

    fix isPrime code

--- isPrime.cpp
+++ isPrime.cpp
@@ -7,8 +7,9 @@ using namespace std;
 Based on the fact that all primes are of the form 6k ± 1
 */
 bool isPrime(unsigned int n) {
-    if (n < 1) return false;
-    if (n < 3) return true;
+    if (n < 2) return false; // 1 is not prime
+    if (n < 4) return true; // 2,3 are prime numbers
+    if (n % 2 == 0 || n % 3 == 0) return false;
     for (int i = 5; i < sqrt(n); i += 6) {
         if (0 == n % i || 0 == n % (i + 2)) return false;
     }
@@ -16,6 +17,11 @@ bool isPrime(unsigned int n) {
 }
 
 int main() {
-    cout << isPrime(982451653);
+
+    for (int i = 0; i < 100; ++i) {
+        if (isPrime(i)) {
+            cout << i << endl;
+        }
+    }
     return 0;
 }
