a17ccbe65c5fe2c48ee261a1ec2f5d11d34f398b
James <classify@gmail.com>
2019-12-03 23:23:49 +0200

    Update consumer_producer.cpp

--- consumer_producer.cpp
+++ consumer_producer.cpp
@@ -15,39 +15,32 @@ void consumer() {
     while (true) {
         unique_lock<mutex> locker(mu);
         cv.wait(locker, []() { return !q.empty(); });
-        int a = q.front();
         cout << "pulling " << q.front() << endl;
         q.pop();
-        locker.unlock();
-        if (a == 5) break;
     }
-    cout << "done" << endl;
 }
 
 void producer() {
-    srand((unsigned) time(nullptr));
-    while (true) {
-        this_thread::sleep_for(chrono::seconds(1));
-        std::mt19937 rng;
-        rng.seed(std::random_device()());
-        std::uniform_int_distribution<std::mt19937::result_type> dist6(1, 10); // distribution in range [1, 6]
-        auto num = dist6(rng);
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(1, 50);
+    int num;
 
-        unique_lock<mutex> locker(mu);
+    while (true) {
+        num = distribution(generator);
+        lock_guard<mutex> lock(mu);
         cout << "pushing " << num << endl;
         q.push(num);
-        locker.unlock();
         cv.notify_one();
-
-        if (num == 5) break;
     }
 }
 
 int main() {
 
+    thread t1(producer);
     thread t2(producer);
-    thread t1(consumer);
-    t2.join();
+    thread t3(consumer);
     t1.join();
+    t2.join();
+    t3.join();
     return 0;
 }
